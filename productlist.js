ddb40242687b1b1788f5a9411ab499dd8bd4ea5a
Line Bøgh <libo1000@stud.ek.dk>
2026-09-22 14:58:03 +0200

    Opdatering af js, og produktliste

---
+++ productlist.js
@@ -0,0 +1,23 @@
+const endpoint = "https://kea-alt-del.dk/t7/api/products";
+const produktliste = document.querySelector(".produktliste");
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then(visData);
+
+function visData(json) {
+  console.log(json);
+  json.forEach((element) => {
+    produktliste.innerHTML += `
+    <a class="link" href="productdetails.html">
+    <article class="card">
+    <img src=https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp alt="produktbillede" />
+    <h2>${element.productdisplayname}</h2>
+    <h3>${element.articletype}</h3>
+    <p>${element.category}</p>
+    <p>${element.price} DKK</p>
+    </article>
+    </a>
+      `;
+  });
+}
